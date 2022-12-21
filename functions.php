5361acf173f8c017bfb49e59382ddc103a0959c5
Thomas Pericoi <thomaspericoi@gmail.com>
2022-12-21 23:07:45 +0100

    Add reset.css

--- functions.php
+++ functions.php
@@ -77,9 +77,11 @@ function enqueue_theme_stylesheets()
         wp_dequeue_style('wp-block-library');
         wp_dequeue_style('dashicons');
     }
+    wp_register_style('reset', get_template_directory_uri() . '/assets/css/reset.css', '', null, 'all');
     wp_register_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css', '', null, 'all');
     wp_register_style('swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.5.8/swiper-bundle.min.css', '', null, 'all');
     wp_register_style('style', get_stylesheet_uri(), '', null, 'all');
+    wp_enqueue_style('reset');
     wp_enqueue_style('font-awesome');
     wp_enqueue_style('swiper');
     wp_enqueue_style('style');
