5bbf75460490ca973991d0f29e2594bff67b45c0
ThomasPericoi <thomaspericoi@gmail.com>
2023-09-15 10:51:57 +0200

    Add OpenDyslexic support

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,28 @@ class Page {
 $(document).ready(function () {
   new Page();
 
-  // General - Quote in the console
+  // General - Insert quote in the console
   console.log(
     "This theme was made by Thomas Pericoi - https://thomaspericoi.com/"
   );
 
   // General - Enable ASCII Printer on random
   printRandomAscii();
+
+  // General - Enable OpenDyslexic toggle
+  document.getElementById("open-dyslexic").addEventListener('change', function () {
+    if (this.checked) {
+      $("html").css({
+        "--body": "OpenDyslexic, sans-serif",
+        "--bold": "OpenDyslexic, sans-serif",
+      });
+      console.log("OpenDyslexic is on");
+    } else {
+      $("html").css({
+        "--body": "Lato, sans-serif",
+        "--bold": "Lato, sans-serif",
+      });
+      console.log("OpenDyslexic is off");
+    }
+  });
 });
--- footer.php
+++ footer.php
@@ -3,6 +3,11 @@
 <!-- Footer -->
 <footer>
     <div class="container">
+        <!-- OpenDyslexic Toggle -->
+        <div>
+            <input type="checkbox" id="open-dyslexic" name="open-dyslexic" />
+            <label for="open-dyslexic">Switch to OpenDyslexic</label>
+        </div>
     </div>
 </footer>
 
