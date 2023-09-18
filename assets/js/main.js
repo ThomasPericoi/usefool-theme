a3423e8eab0c6d051d4bd6bb274a5bc220f7fccf
ThomasPericoi <thomaspericoi@gmail.com>
2023-09-18 16:01:34 +0200

    JS | OpenDyslexic session added

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,19 +26,37 @@ $(document).ready(function () {
   printRandomAscii();
 
   // General - Enable OpenDyslexic toggle
+  function enableDyslexicMode() {
+    $("html").css({
+      "--body": "OpenDyslexic, sans-serif",
+      "--bold": "OpenDyslexic, sans-serif",
+    });
+    sessionStorage.setItem("dyslexicMode", true);
+    console.log("OpenDyslexic est activé");
+  }
+
+  function disableDyslexicMode() {
+    $("html").css({
+      "--body": "Ubuntu, sans-serif",
+      "--bold": "Ubuntu, sans-serif",
+    });
+    sessionStorage.setItem("dyslexicMode", false);
+    console.log("OpenDyslexic est désactivé");
+  }
+
+  if (sessionStorage.getItem("dyslexicMode") == "true") {
+    enableDyslexicMode();
+    document.getElementById("open-dyslexic").checked = true;
+  } else {
+    disableDyslexicMode();
+    document.getElementById("open-dyslexic").checked = false;
+  };
+
   document.getElementById("open-dyslexic").addEventListener('change', function () {
     if (this.checked) {
-      $("html").css({
-        "--body": "OpenDyslexic, sans-serif",
-        "--bold": "OpenDyslexic, sans-serif",
-      });
-      console.log("OpenDyslexic is on");
+      enableDyslexicMode();
     } else {
-      $("html").css({
-        "--body": "Lato, sans-serif",
-        "--bold": "Lato, sans-serif",
-      });
-      console.log("OpenDyslexic is off");
+      disableDyslexicMode();
     }
   });
 });
